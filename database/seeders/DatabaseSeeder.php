9dfa1340a35fb3ebec84773c1d2fa1bac05079f2
francine265 <117289117+francine265@users.noreply.github.com>
2023-09-19 15:57:26 -0400

    jj
    k

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(DepartementsTableSeeder::class);
+        $this->call(UsagersTableSeeder::class);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
