d6c479bc7b5ee0961c5e861a11693cbb7f606261
papystanny <117280280+papystanny@users.noreply.github.com>
2023-09-20 10:51:27 -0400

    ESSAI DE LA PAGE DE CONNEXION
    2023/09/20

--- ProjetIntegartion/resources/views/Connexion/connect.blade.php
+++ ProjetIntegartion/resources/views/Connexion/connect.blade.php
@@ -5,7 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script defer src="https://use.fontawesome.com/releases/v5.1.0/js/all.js" integrity="sha384-3LK/3kTpDE/Pkp8gTNp2gR/2gOiwQ6QaO7Td0zV76UFJVhqLl4Vl3KL1We6q6wR9" crossorigin="anonymous"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
     <link rel="stylesheet" href="{{asset('style.css')}}">
     <title> Page de Connexion</title>
--- ProjetIntegartion/resources/views/layouts/app.blade.php
+++ ProjetIntegartion/resources/views/layouts/app.blade.php
@@ -4,40 +4,33 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{asset('style.css')}}">
-    <link rel="stylesheet" media="(max-width: 768px)" href="styles.css">
-
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{asset('css/layout.css')}}">
+    <link rel="stylesheet" media="(max-width: 768px)" href="css/layout.css">
+
+  
      <title>@yield('titre')</title>
 </head>
 <body>
-        <!-- HEADER -->
-        <header>
-        <nav class="main-nav"> 
-        <div class="container-fluid parent">
-                <div class="row  text-center h-100 border ">
-                        <div class="">
-                           <img src="img/examen.png" class="fluid" width="100px" height="100px"><br>
-                       </div>
-                       <div class="">
-                          <h6>TRANSPORT DE MATERIAUX</h6>
-                       </div>
-                       <div class=" bg">
-                          <i class="fa-solid fa-user fa-6x"></i>
-                       </div>   
-                       <div class="">
-                       <form method=""> 
-                          <button type="submit" class="btnConnexion">Se déconnecter</button>
-                        </form>
-                       </div> 
-                       
-                </div>
-        </div> 
-      </nav>
-     
-    </header>
-    <!-- END OF HEADER -->
-    <br><br><br>
+   
+<!-- HEADER -->
+            <header>
+               <nav class="main-nav">
+                  <div class="container-fluid">
+                     <div class="row">
+                           <div class="col-sm-2  p-0">
+                              <img src="img/examen.png" class="img-fluid logo-marge-10" width="80px" height="80px">
+                           </div>
+
+                           <div class="col-sm-2    p-0">
+                              <img src="img/examen.png" class="img-fluid logo-marge-10" width="80px" height="80px">
+                           </div>
+                     </div>
+                  </div>
+               </nav>
+            </header>
+<!-- END OF HEADER -->
+    
  
     @yield('contenu')
 
@@ -50,6 +43,8 @@
       <p>&copy 1997-2018 Netflix, Inc.</p>
     </footer>
    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  
 </body>
 </html>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
