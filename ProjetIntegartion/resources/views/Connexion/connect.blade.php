c886a9ebccc518ba19326d5dc1f60739e305a726
papystanny <117280280+papystanny@users.noreply.github.com>
2023-09-24 19:21:02 -0400

    Connexion | STAN DIMANCHE
    - Debut de la page connexion
    - Layout
    DIMANCHE | 2023-09-24

--- ProjetIntegartion/resources/views/Connexion/connect.blade.php
+++ ProjetIntegartion/resources/views/Connexion/connect.blade.php
@@ -4,34 +4,29 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Connexion</title>
     <script defer src="https://use.fontawesome.com/releases/v5.1.0/js/all.js" integrity="sha384-3LK/3kTpDE/Pkp8gTNp2gR/2gOiwQ6QaO7Td0zV76UFJVhqLl4Vl3KL1We6q6wR9" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
-    <link rel="stylesheet" href="{{asset('style.css')}}">
+    <link rel="stylesheet" href="{{asset('css/login/login.css')}}">
+    <link rel="icon" href="https://tse1.mm.bing.net/th?id=OIP.BvE9Kz_K4pOY9ceOf4bLIQHaEK&pid=Api" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+  
     <title> Page de Connexion</title>
 </head>
 <body>
     @extends('layouts.app')
 
     @section('contenu')
-    <section class="main-container  align-items-center text-center h-100  py-5">
-        <div class="container containerForm">
-            <div style="width:50%;">
-                <h1 class="titreForm">Connexion</h1>
-                    <form method="POST"action="{{route('Formulaires.login')}}">
-                        @csrf
-                        <div class="mb-3 ">
-                            <input type="id" class="form-control" id="id" name="id" aria-describedby="emailHelp" placeholder="id" required>
-                        </div>
-                        <div class="mb-3 py-3">
-                            <input type="password" class="form-control" id="password" name="password" placeholder="Password" required>
-                        </div>
-                        <button type="submit" class="btn btn-primary">Se Connecter</button>
 
-                    </form>
-                </h1>
-            </div>
+   
+        <div class="container">
+             <div class="">
+                
+            </div> 
         </div>
-    </section>
+
+  
+ 
     @endsection
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
     
--- ProjetIntegartion/resources/views/layouts/app.blade.php
+++ ProjetIntegartion/resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="{{asset('css/layout.css')}}">
+    <link rel="stylesheet" href="{{asset('css/hamburger.css')}}">
     <link rel="stylesheet" media="(max-width: 768px)" href="css/layout.css">
 
   
@@ -13,38 +14,73 @@
 </head>
 <body>
    
-<!-- HEADER -->
-            <header>
-               <nav class="main-nav">
-                  <div class="container-fluid">
-                     <div class="row">
-                           <div class="col-sm-2  p-0">
-                              <img src="img/examen.png" class="img-fluid logo-marge-10" width="80px" height="80px">
-                           </div>
+<div class="prenav-in" style="display:none;">
+   <h7 class="" href="#"><i class="fas fa-sign-out-alt"></i><u> SE DÉCONNECTER </u></h7></li>          
+</div> 
 
-                           <div class="col-sm-2    p-0">
-                              <img src="img/examen.png" class="img-fluid logo-marge-10" width="80px" height="80px">
-                           </div>
-                     </div>
-                  </div>
-               </nav>
-            </header>
-<!-- END OF HEADER -->
-    
- 
+
+
+<nav class="navbar">
+    <div class="left-section">
+
+        <div class="logo">
+            <img src="img/examen.png"  class="img-fluid logo-marge-10 " width="50px" height="50px">
+        </div>
+
+        <div class="department-name" style="display:none;">
+            <span class="department ">SÉCURITÉ</span>
+            <span class="employee"> John Doe</span>
+        </div>
+
+    </div>
+
+    <div class="right-section" >
+
+         <div class="center-section" style="display:none;">
+            <div class="user-notifications">
+                  <img src="img/userImg.png" class="img-fluid logo-marge-10" width="50" height="50px">
+            </div>
+         </div>
+
+         <div class="hamburger-menu" style="display:none;">
+            <input id="menu__toggle" type="checkbox" style="display:none;"/>
+            <label class="menu__btn" for="menu__toggle">
+               <span></span>
+            </label>
+
+            <ul class="menu__box">
+               <li><a class="menu__item" href="#"><i class="fas fa-home"></i> ACCUEIL </a></li>
+               <hr style="color: black;" />
+               <li><a class="menu__item" href="#"><i class="far fa-clock"></i> FORMULAIRES </a></li>
+               <hr style="color: black;" />
+               <li><a class="menu__item" href="#"><i class="fas fa-folder"></i> MES EMPLOYES </a></li>
+               <hr style="color: black;" />
+               <li><a class="menu__item" href="#"><i class="fas fa-sign-out-alt"></i> SE DÉCONNECTER </a></li>
+              
+            </ul>
+         </div>
+
+         <div class="div-connexion">
+             <h3><strong> CONNEXION </strong> </h3>
+         </div> 
+
+    </div>
+</nav>
+
+
+
     @yield('contenu')
 
     
     <!-- END OF LINKS -->
 
     <!-- FOOTER -->
-    <footer>
-    <div class="row align-items-center text-center h-100  py-5">
-      <p>&copy 1997-2018 Netflix, Inc.</p>
-    </footer>
-   </div>
+   
+
   
 </body>
 </html>
 
+
+<script src="js/hamburger.js" defer></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
