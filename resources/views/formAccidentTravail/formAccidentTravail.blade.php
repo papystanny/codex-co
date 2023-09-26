d30833b8ad3cb7983f8e05b4e2a4e31745320b47
francine265 <117289117+francine265@users.noreply.github.com>
2023-09-26 14:20:13 -0400

    g
    d

--- resources/views/formAccidentTravail/formAccidentTravail.blade.php
+++ resources/views/formAccidentTravail/formAccidentTravail.blade.php
@@ -22,9 +22,10 @@
   </div>
   <div class="form-check">
   <label for="temoins">Témoin(s)</label> 
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+
+   <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
     <label class="form-check-label" for="exampleCheck1">oui</label>
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
     <label class="form-check-label" for="exampleCheck1">non</label>
   </div>
   <div class="form-group">
@@ -32,9 +33,87 @@
     <input type="text" class="form-control" id="formGroupExampleInput" placeholder="">
   </div>
   <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    <label for="formGroupExampleInput">endroit de l'accident</label>
+    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="">
   </div>
+  <div class="form-group">
+    <label for="formGroupExampleInput">secteur d'activité</label>
+    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="">
+  </div>
+  <div class="form-group">
+    <label for="formGroupExampleInput">Nature et site de la blessure(cochez s'il y'a lieu côté gauche ou droit)</label>
+  </div>
+  <div class="form-check">
+    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+    <label class="form-check-label" for="flexCheckDefault">
+       tête,visage,nez,yeux,oreilles
+    </label>
+    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+    <label class="form-check-label" for="flexCheckDefault">
+      D/
+    </label>
+    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+    <label class="form-check-label" for="flexCheckDefault">
+     G
+    </label>
+  </div>
+  <div class="form-check">
+    <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
+    <label class="form-check-label" for="flexCheckChecked">
+       Torse
+    </label>
+  </div>
+  <div class="form-check">
+    <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
+    <label class="form-check-label" for="flexCheckChecked">
+       Poumons
+    </label>
+  </div>
+  <div class="form-check">
+    <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
+    <label class="form-check-label" for="flexCheckChecked">
+       Bras,épaules,coudes
+    </label>
+    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+    <label class="form-check-label" for="flexCheckDefault">
+      D/
+    </label>
+    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+    <label class="form-check-label" for="flexCheckDefault">
+     G
+    </label>
+  </div>
+  <div class="form-check">
+    <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
+    <label class="form-check-label" for="flexCheckChecked">
+    Poignets,mains,doigts
+    </label>
+    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+    <label class="form-check-label" for="flexCheckDefault">
+      D/
+    </label>
+    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+    <label class="form-check-label" for="flexCheckDefault">
+     G
+    </label>
+  </div>
+  <div class="form-check">
+    <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
+    <label class="form-check-label" for="flexCheckChecked">
+       Dos
+    </label>
+  </div>
+
+  <div class="form-check">
+    <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+    <label class="form-check-label" for="flexCheckChecked">
+        Checked checkbox
+    </label>
+  </div>
+
+
+
+ 
   <div class="form-check">
     <input type="checkbox" class="form-check-input" id="exampleCheck1">
     <label class="form-check-label" for="exampleCheck1">Check me out</label>
@@ -42,4 +121,6 @@
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>    
 </div>
+
+
 @endsection
