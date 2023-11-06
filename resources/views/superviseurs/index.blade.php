e6c3af8cccf76e4404d6884b705ad0330345b18b
francine265 <117289117+francine265@users.noreply.github.com>
2023-11-06 14:13:43 -0500

    res
    res

--- resources/views/superviseurs/index.blade.php
+++ resources/views/superviseurs/index.blade.php
@@ -15,4 +15,21 @@
         </div>
     </div>
 </div>
+<div class="contsiner-fluid">
+    <div class="row ">
+        <div class="col-md-8">
+            <h1><b>notifications</b></h1>  
+   <!--         @foreach ($data as $item)
+    @if ($item->nom_colonne === 'valeur_recherchee')
+        <p>La valeur recherchée existe dans la base de données.</p>
+    @else
+        <p>La valeur recherchée n'existe pas dans la base de données.</p>
+    @endif
+@endforeach
+-->
+            <br>
+        </div>
+    </div>
+
+</div>
 @endsection
