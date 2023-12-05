5f18c8172df3be7610b0160b25b93a1d1ce8b5f9
JoyceVanelle <tsatiamomo@gmail.com>
2023-12-05 12:28:34 -0500

    Ma partie est terminer
    -les differentes formulaire s'enregistre
    -les usager qui remplises des formulaire , on est capable de leur voir dans leur table quil ont enregistrer un formulaire defini.
    - il ya un index.php dans la vue qui permets d'afficher les details de tous les formulaire (mes 2 formulaires)
    -les notifications marche
    - breff tout est bon comme convenu

--- ProjetIntegartion/resources/views/Formulaires/index.blade.php
+++ ProjetIntegartion/resources/views/Formulaires/index.blade.php
@@ -23,73 +23,72 @@
                             <div class="formMec">
                                 <div class="formMecDetails">
                                 
-    <form >
-            
+    <form >    
             <div class = " center">
-                <h3  class="titreForm col-8 p-4">
+                <h3  class="titreForm col-8 p-2">
                 1.Numéro(s) d'unités(s) impliqué(s)
                  </h3>         
             </div>
-            <div class = " center">
+            <div class = "titreForm col-8 p-2">
 
              <div class="numUniteImplique">{{$formMec->numUniteImplique}}</div>
                       
             </div>
-            <div class = " center">
-                <h3  class="titreForm col-6 p-4">
+            <div class = "titreForm col-8 p-2">
+                <h3  class="titreForm col-8 p-2">
                 2.Département* 
                  </h3>         
             </div>
-            <div class = " center">
+            <div class = "titreForm col-8 p-2">
             <div class="departement">{{$formMec->departement}}</div>  
             </div>     
-            <div class = " center">
-                <h3  class="titreForm col-8 p-4">
+            <div class = " titreForm col-8 p-2">
+                <h3  class="titreForm col-8 p-2">
                 3.Prenom et Nom de l'employé impliqué
                  </h3>  
-                 <div class = " center">
+                 <div class = "titreForm col-8 p-2">
             <div class="departement">{{$formMec->nomEmploye}}</div>  
             </div>        
             </div>
                
-            <div class = " center">
-                <h3  class="titreForm col-8 p-4">
+            <div class = "titreForm col-8 p-2">
+                <h3  class="titreForm col-8 p-2">
                 4.Prenom et Nom du supérieur immédiat 
                  </h3>         
             </div>
-            <div class = " center">
+            <div class = "titreForm col-8 p-2">
             <div class="prenomNomSupImmediat">{{$formMec->prenomNomSupImmediat}}</div>
             </div>      
-            <div class = " center">
-                <h3  class="titreForm col-9 p-4">
+            <div class = "titreForm col-8 p-2">
+                <h3  class="titreForm col-8 p-2">
                 5.Numéro du permis de conduire de l'employé 
                  </h3>         
             </div>
-            <div class = " center">
+            <div class = "titreForm col-8 p-2">
             <div class="numPermisConduireEmploye">{{$formMec->numPermisConduireEmploye}}</div>
             </div> 
-            <div class = " center">
-                <h3  class="titreForm col-8 p-4">
+            <div class = "titreForm col-8 p-2">
+                <h3  class="titreForm col-8 p-2">
                 6.Autres Véhicules impliqués(Citoyen) 
                  </h3>         
             </div>   
-            <div class = " center titreForm col-6 p-2">
+            <div class = " titreForm col-8 p-2">
             <div class="vehiculeCityonImplique">{{$formMec->vehiculeCityonImplique}}</div>
             </div>  
-            <div class = " center">
-                <h3  class="titreForm col-9 p-4">
+            <div class = "titreForm col-8 p-2">
+                <h3  class="titreForm col-8 p-2">
                 7.Le superviseur a été aviser? 
                  </h3>         
             </div>
-            <div class = " center">
+            <div class = "titreForm col-8 p-2">
             <div class="notifSup">{{$formMec->notifSup}}</div>
             </div> 
-            <div class = " center">
-                <h3  class="titreForm col-9 p-4">
+            <div class = "titreForm col-8 p-2">
+                <h3  class="titreForm col-8 p-2">
                 8.l'admin a été aviser? 
                  </h3>         
             </div>
-            <div class = " center">
+            <div class = "titreForm col-8 p-2">
             <div class="notifAdmin">{{$formMec->notifAdmin}}</div>
             </div> 
                                  
@@ -99,14 +98,90 @@
 </div>
   @endforeach
                     </div>
-                    
-@section('contenu')    
-    @if(count($formMec))
-
+                    <h3 id="home">Liste des Formulaires de Situation Dangereuses</h3>
                     <div class="div">
                         @foreach($formsit as $formsit)
                             <div class="formSit">
-                                <div class="formSitDetails">
+                            <div class="formSitDetails">      
+          <div class="parent container-fluid">
+          
+             <div class="row">
+                <div class= "titreForm col-8 p-2">
+                    <label for="fname">Nom:</label><br>
+                    <div class="nomEmploye">{{$formsit->nomEmploye}}</div>
+                </div>
+               <div class= "titreForm col-8 p-2">
+                    <label for="fname">Prenom:</label><br>
+                    <div class="prenomEmploye">{{$formsit->prenomEmploye}}</div>
+                </div>
+                <div class= "titreForm col-8 p-2">
+                    <label for="fname">Matricule:</label><br>
+                    <div class="matriculeEmploye">{{$formsit->matriculeEmploye}}</div>
+                </div>
+             </div>
+          </div>
+
+          <div class="parent container-fluid">
+                <div class="row">
+                        <div class= "titreForm col-8 p-2">
+                            <label for="fname">Fonction au moment de l'evenement:</label>
+                            <div class="fonctionLorsEvenement">{{$formsit->fonctionLorsEvenement}}</div>
+                        </div>
+                        <div class= "titreForm col-8 p-2">
+                            <label for="fname">Secteur d'activité:</label><br>
+                            <div class="secteurActivite">{{$formsit->secteurActivite}}</div>
+                        </div>
+                </div>
+           </div>
+          <div class="parent container-fluid">
+                
+                <div class="row">
+                        <div class= "titreForm col-8 p-2">
+                        <div class="dateObservation">{{$formsit->dateObservation}}</div>
+                        </div>
+                </div>
+          </div>
+
+        <div class="parent container-fluid">
+            <div class="row">
+                    <div class= "titreForm col-8 p-2">                              
+                    
+                        <label for="Date">Date de L'Observation:</label>
+                        <div class="heureObservation">{{$formsit->heureObservation}}</div>
+                    </div>
+                    <div class= "titreForm col-8 p-2">
+                        
+                            <label for="heure">Heure de L'Observation:</label><br>
+                            <div class="temoins">{{$formsit->temoins}}</div>
+                    
+                    </div>
+                    <div class= "titreForm col-8 p-2">
+                    
+                        <label for="fname">Témoin(s):</label>
+                        <div class="descriptionEvenement">{{$formsit->descriptionEvenement}}</div>
+            </div>
+        </div>
+     
+          <div class="titreForm col-8 p-2">
+                <div class="row">
+                    <label for="fname">Correction(s) ou amélioration(s) proposée(s)</label>
+                    <div class="ameliorationsProposees">{{$formsit->ameliorationsProposees}}</div> 
+                </div>
+         </div>
+        
+         
+         <div class="titreForm col-8 p-2">
+                <div class="row">
+                <label for="fname">dateSupeAvise:</label>
+                    <div class="dateSupeAvise">{{$formsit->dateSupeAvise}}</div>
+                </div>
+         </div>
+       
+         <!-- <div class="parent container-fluid">
+                <div class="row ">
+                    <div class= " color enfant enfant1 col-lg-12 ">&nbsp</div>      
+                </div>
+          </div>
                                     <div class="id">{{$formsit->id}}</div>
                                     <div class="nomEmploye">{{$formsit->nomEmployed}}</div>
                                     <div class="prenomEmploye">{{$formsit->prenomEmploye}}</div>
@@ -128,17 +203,14 @@
                                     <div class="numPosteSuperviseur">{{$formsit->numPosteSuperviseur}}</div>
                                     <div class="notifSup">{{$formsit->notifSup}}</div>
                                     <div class="notifAdmin">{{$formsit->notifAdmin}}</div>
-                                    <div class="created_at">{{$formsit->created_at}}</div>
-                                    <div class="updated_at">{{$formsit->updated_at}}</div>               
+                                                 
                                 </div>
-                            </div>
+                            </div> -->
                         @endforeach
                     </div>
                 </div>
         </section>
     @endif
 @endsection
-
-    
 </body>
 </html>
